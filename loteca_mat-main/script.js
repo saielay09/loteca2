a4910286e5ea7771840ab9566095e2e321ee5bec
mateussamurio <jorge.samurio@estudante.ifms.edu.br>
2022-03-15 11:37:08 -0300

    final do script

---
+++ loteca_mat-main/script.js
@@ -0,0 +1,42 @@
+var numSort = []
+var numEsco = []
+
+function sorteio(){
+    var cont = 0
+    numSort=[]
+
+    while(cont < 6){
+        let num = Math.random() * 60
+        num = Math.ceil(num)
+        if(!numSort.includes(num)){
+            numSort[cont] = num
+            console.log(numSort)
+            cont++
+        }
+    }
+    document.getElementById("sorteados").innerHTML = numSort
+    contAcertos()
+}
+
+function getValor(valor, pos){
+    valor = Number(valor)
+    if(valor < 0 || valor > 60){
+        alert("Número invalido, digite um entre 1 e 60")
+        document.getElementById(`num${pos+1}`).value = ""
+    }else if (numEsco.includes(valor)){
+        alert("Número repetido, escolha um outro número")
+        document.getElementById(`num${pos+1}`).value = ""
+    }else {
+        numEsco[pos] = valor
+        console.log(numEsco)
+    }
+}
+ function contAcertos(){
+     let contA = 0
+     numEsco.forEach(function(value,index){
+         if (numSort.includes (value)) {
+             contA = contA + 1
+         }
+     })
+     document.getElementById("acertos").innerHTML = contA
+ }
