89e084ad16bf0ed3bc1b76aa73131d4e6d3f864c
Estefany Ciely <101192829+saielay09@users.noreply.github.com>
2022-04-05 11:27:58 -0300

    Update script.js

--- loteca_mat-main/script.js
+++ loteca_mat-main/script.js
@@ -1,47 +1,65 @@
-var numSort = []
-var numEsco = []
+var numSort = [];
+var cont = 0;
+var numEsco =[];
 
-function sorteio() {
-    if (numEsco.lenght == 6) {
-        var cont = 0
-        numSort = []
-
-        while (cont < 6) {
+function sorteio (){
+    if(numEsco.length == 6){
+        var cont = 0;
+        numSort=[]
+        while(cont < 6){
             let num = Math.random() * 60
-            num = Math.ceil(num)
-            if (!numSort.includes(num)) {
-                numSort[cont] = num
+            num = Math.ceil(num);
+            if(!numSort.includes(num)){
+                numSort[cont] = num;
                 console.log(numSort)
                 cont++
             }
-        }
-        document.getElementById("sorteados").innerHTML = numSort
-        contAcertos()
+       
+        } 
+    document.getElementById("sorteados").innerHTML = numSort;
+    contAcertos();
     }else{
-        alert("Faltam número a serem colocados, preencha todos os seis campos.")
+        alert("É necessário digitar os seis numeros antes de sortear")
     }
-
+    
 }
-
-function getValor(valor, pos) {
-    valor = Number(valor)
-    if (valor < 0 || valor > 60) {
-        alert("Número invalido, digite um entre 1 e 60")
-        document.getElementById(`num${pos + 1}`).value = ""
-    } else if (numEsco.includes(valor)) {
-        alert("Número repetido, escolha um outro númera")
-        document.getElementById(`num${pos + 1}`).value = ""
-    } else {
+function getValor(valor, pos){
+    valor = Number(valor);
+    if (valor <= 0 || valor > 60){
+        alert("numero inválido, digite um numero entre 1 e 60");
+        document.getElementById(`num${pos+1}`).value = "";
+    }else if(numEsco.includes(valor)){
+        alert("numero repetido, escolha um outro número")
+        document.getElementById(`num${pos+1}`).value = "";
+    }else{
         numEsco[pos] = valor
-        console.log(numEsco)
-    }
+    console.log(numEsco)
+    } 
 }
-function contAcertos() {
+
+function contAcertos(){
     let contA = 0
-    numEsco.forEach(function (value, index) {
-        if (numSort.includes(value)) {
-            contA = contA + 1
-        }
+    numEsco.forEach(function(value,index){
+        if (numSort.includes(value)){
+        contA  = contA + 1
+
+    }
+    
+        
     })
-    document.getElementById("acertos").innerHTML = contA
+
+
+document.getElementById("acertos").innerHTML= contA
 }
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
